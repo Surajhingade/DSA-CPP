d3aad1375dfd21d4697a7d6f2b61b22e3b754b3c
go <gohp1999@gmail.com>
2021-12-28 18:24:39 +0530

    Create CharacterArray.cpp
    Create simple array

---
+++ CharacterArray.cpp
@@ -0,0 +1,18 @@
+// Character Array and String deep down.
+
+#include<iostream>
+using namespace std;
+
+
+
+int main() {
+ // creating character array 
+ char name[20];
+ cout<<"Enter your name"<<endl;
+ cin>>name;
+ cout<<"Your Name is " + name;
+
+
+
+    return 0;
+}
