2a72054954ebf9c0a5c2987cbc6360757471b552
go <hingadesuraj7620@gmail.com>
2021-12-28 18:45:19 +0530

    Update CharacterArray.cpp
    Calculate Length of character array.

--- CharacterArray.cpp
+++ CharacterArray.cpp
@@ -3,6 +3,15 @@
 #include<iostream>
 using namespace std;
 
+// Calculate length of character array
+
+int getCharArrayLength(char name[]){
+    int count = 0;
+    for(int i=0; name[i] != '\0'){
+        count++;
+    }
+    return count;
+}
 
 
 int main() {
@@ -11,7 +20,12 @@ int main() {
  cout<<"Enter your name"<<endl;
  cin>>name;
  cout<<"Your Name is " + name;
+ cout<<"Length of Character array name is : "<<getCharArrayLength(name)<<endl;
+ 
 
+ // output is 
+ // Your Name is hello
+ // Length of Character array name is : 5 
 
 
 
