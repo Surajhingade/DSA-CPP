67ede358a7266ce7c94e17e7fdeb79c0c0850de9
go <hingadesuraj7620@gmail.com>
2021-12-28 19:58:46 +0530

    Update CharacterArray.cpp
    Given string is Palindrome or not

--- CharacterArray.cpp
+++ CharacterArray.cpp
@@ -3,6 +3,23 @@
 #include<iostream>
 using namespace std;
 
+// check string array is palindram or not
+
+bool checkPalindrome(char name[] , int n){
+    int start = 0;
+    int end = n-1;
+
+    if(name[start] != name[end]){
+        return 0;
+    }
+    else{
+        start++;
+        end--;
+    }
+    return 1;
+}
+
+
 // reverse a string function
 
 void reverseString(char name[], int n){
@@ -33,14 +50,15 @@ int main() {
  cout<<"Enter your name"<<endl;
  cin>>name;
  cout<<"Your Name is " + name;
+
+
  cout<<"Length of Character array name is : "<<getCharArrayLength(name)<<endl;
  
-
  // output is 
  // Your Name is hello
  // Length of Character array name is : 5 
 
-
+cout<<"checkPalindrome is or not"<<checkPalindrome(name,getCharArrayLength(name));
 
     return 0;
 }
