c144a28ac05395108f202cb6580a84b326816eef
go <hingadesuraj7620@gmail.com>
2022-01-01 23:50:11 +0530

    Update recursion1.cpp
    add 1 more recursive function for find power of any number

--- recursion1.cpp
+++ recursion1.cpp
@@ -5,7 +5,7 @@
 #include<iostream>
 using namespace std;
 
-// recursive function
+// recursive function for find factorial 
 
 int factorial(n){
 
@@ -23,15 +23,41 @@ int factorial(n){
 
 
 
+// recursive function for find power on (n) tern
+
+
+int power(int n){
+    // base case power of any number 0 = 1;
+    if(n==0)
+        return 1;
+
+
+        int smallproblem = power(n-1);
+        int bigproblem = 2 * smallproblem;
+
+
+        return bigproblem;
+}
+
+
+
 int main(){
+
+
 
 int n;
 cin>>n;
 
-
+// function call for find factorial of any number.
 int ans = factorial(n);
 
 cout<<"the factorial of number"<<n<<"is"<<ans<<endl;
+
+// fucntion call for find power of any number.
+
+int ans2 = power(n);
+
+cout<<"the power of " << n << "is"<<ans2<<endl;
 
 
 
